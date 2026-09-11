a377b8e4f2355f0ce089a34564e3f36bb733874d
samaiira <samairaayadav@gmail.com>
2026-09-11 15:54:02 +0000

    Fix API server TypeScript imports

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import pinoHttpModule from "pino-http";
 import { clerkMiddleware } from "@clerk/express";
 import { publishableKeyFromHost } from "@clerk/shared/keys";
 import {
@@ -11,20 +11,21 @@ import {
 import router from "./routes";
 import { logger } from "./lib/logger";
 
-const app: Express = express();
+const pinoHttp = pinoHttpModule as any;
 
+const app: Express = express();
 app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
